b9bffab0ab498087fa88833665a6b3b9dc2f55e7
hero2bash <zero.bash@gmail.com>
2018-06-26 23:02:27 +0800

    Add functions for individual note surface generation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from sys import exit
 from pygame import Color, Rect
 from pygame.locals import *
@@ -10,12 +11,14 @@ screen = pygame.display.set_mode((800, 600))
 colors = {
     'BLACK': Color(0, 0, 0),
     'WHITE': Color(255, 255, 255),
+    'MAGENTA': Color(255, 0, 255),
 }
 
 STAFF_LINE_INTERVAL = 30
 STAFF_LINE_PIXEL_WIDTH = 2
 
 notes_dict = {}
+
 
 def main():
     # Game Loop
@@ -30,12 +33,13 @@ def event_update():
             pygame.quit()
             exit()
 
-
 def display_update():
-    screen.fill(Color(255, 255, 255))
+    screen.fill(colors['WHITE'])
 
     draw_staff(500, (20, 20))
+    fill_notes_dict(0.75)
 
+    draw_note('32', screen)
     pygame.display.update()
 
 
@@ -57,17 +61,111 @@ def draw_staff(width, start_pos=(0, 0)):
 
 
 def fill_notes_dict(scale=1.0):
-    note_width = 200 * scale
-    note_height = 300 * scale
+    note_width = 100 * scale
+    note_height = 125 * scale
     note_size = (note_width, note_height)
 
-    whole_note = pygame.Surface(note_size)
-    pygame.draw.ellipse(whole_note,
+    notes_dict.setdefault('1', draw_whole_note(note_size))
+    notes_dict.setdefault('2', draw_half_note(note_size))
+    notes_dict.setdefault('4', draw_quarter_note(note_size))
+    notes_dict.setdefault('8', draw_eighth_note(note_size))
+    notes_dict.setdefault('16', draw_sixteenth_note(note_size))
+    notes_dict.setdefault('32', draw_thirty_second_note(note_size))
+
+
+# Returns pygame surface of a whole note
+def draw_whole_note(note_size, filled=False):
+    note_width = note_size[0]
+    note_height = note_size[1]
+    note = pygame.Surface(note_size)
+    note.fill(colors['MAGENTA'])
+    note.set_colorkey(colors['MAGENTA'])
+    pygame.draw.ellipse(note,
                         colors['BLACK'],
                         Rect((0, (2 * note_height) / 3),
-                            (note_width / 2, note_height / 3)),
-                        STAFF_LINE_PIXEL_WIDTH)
-    notes_dict.setdefault('whole_note', whole_note)
+                             (note_width / 2, note_height / 3)),
+                        # Decision for filling based on note being built
+                        STAFF_LINE_PIXEL_WIDTH if not filled else 0)
+    return note
+
+
+def draw_half_note(note_size):
+    note_width = note_size[0]
+    note_height = note_size[1]
+    note = pygame.Surface(note_size)
+    note.fill(colors['MAGENTA'])
+    note.set_colorkey(colors['MAGENTA'])
+    note.blit(draw_whole_note(note_size), (0, 0))
+    pygame.draw.line(note,
+                     colors['BLACK'],
+                     (note_width / 2.1, (5 * note_height) / 6),
+                     (note_width / 2.1, 0),
+                     STAFF_LINE_PIXEL_WIDTH)
+    return note
+
+
+def draw_quarter_note(note_size):
+    note_width = note_size[0]
+    note_height = note_size[1]
+    note = pygame.Surface(note_size)
+    note.fill(colors['MAGENTA'])
+    note.set_colorkey(colors['MAGENTA'])
+    note.blit(draw_whole_note(note_size, True), (0, 0))
+    pygame.draw.line(note,
+                     colors['BLACK'],
+                     (note_width / 2.1, (5 * note_height) / 6),
+                     (note_width / 2.1, 0),
+                     STAFF_LINE_PIXEL_WIDTH)
+    return note
+
+
+def draw_eighth_note(note_size):
+    note_width = note_size[0]
+    note_height = note_size[1]
+    note = pygame.Surface(note_size)
+    note.fill(colors['MAGENTA'])
+    note.set_colorkey(colors['MAGENTA'])
+    note.blit(draw_quarter_note(note_size), (0, 0))
+    pygame.draw.line(note,
+                     colors['BLACK'],
+                     (note_width / 2.1, 0),
+                     (note_width, note_height / 8),
+                     int(STAFF_LINE_PIXEL_WIDTH * 3))
+    return note
+
+
+def draw_sixteenth_note(note_size):
+    note_width = note_size[0]
+    note_height = note_size[1]
+    note = pygame.Surface(note_size)
+    note.fill(colors['MAGENTA'])
+    note.set_colorkey(colors['MAGENTA'])
+    note.blit(draw_eighth_note(note_size), (0, 0))
+    pygame.draw.line(note,
+                     colors['BLACK'],
+                     (note_width / 2.1, note_height / 8),
+                     (note_width, (note_height / 8) * 2),
+                     int(STAFF_LINE_PIXEL_WIDTH * 3))
+    return note
+
+
+def draw_thirty_second_note(note_size):
+    note_width = note_size[0]
+    note_height = note_size[1]
+    note = pygame.Surface(note_size)
+    note.fill(colors['MAGENTA'])
+    note.set_colorkey(colors['MAGENTA'])
+    note.blit(draw_sixteenth_note(note_size), (0, 0))
+    pygame.draw.line(note,
+                     colors['BLACK'],
+                     (note_width / 2.1, (note_height / 8) * 2),
+                     (note_width, (note_height / 8) * 3),
+                     int(STAFF_LINE_PIXEL_WIDTH * 3))
+    return note
+
+
+def draw_note(note_to_draw, surface, pos=(0, 0)):
+    surface.blit(notes_dict[note_to_draw], pos)
 
 
 if __name__ == '__main__':
